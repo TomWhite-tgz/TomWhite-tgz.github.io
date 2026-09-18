e6cd241c8344cf8c0abb49ab4108e1239dfc7617
tomwhite <TomWhitetgz@outlook.com>
2026-09-18 21:01:48 +0800

    Prepare Astro homepage for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    // 换成你的 github pages 完整网址
+    site: 'https://TomWhite-tgz.github.io',
+    devToolbar: {
+        enabled: false,
+    },
+});
